c0ea7dfc6590fdd59f629eb159564b8e44be8ec5
hessam-shm <hessam@ymail.com>
2021-05-16 12:40:31 +0430

    ui show error for more than 2 players

--- frontend/src/component/Board.js
+++ frontend/src/component/Board.js
@@ -6,34 +6,39 @@ import Bank from "./Bank";
 class Board extends Component {
 
     render() {
-        const pits = Object.assign([],this.props.pits);
-        const players = Object.assign([],this.props.players);
+        const pits = Object.assign([], this.props.pits);
+        const numberOfPlayers = Object.assign([], this.props.players).length;
         const move = this.props.move;
 
         //works only if players are 2
-        const numberOfPitsForEachPlayer = (pits.length/2)-1;
-        const player1Pits = pits.slice(0,(pits.length/2)-1);
-        const player1Bank = pits[(pits.length/2)-1];
-        const player2Pits = pits.slice(pits.length/2,pits.length-1);
-        const player2Bank = pits[pits.length-1];
+        const numberOfPitsForEachPlayer = (pits.length / 2) - 1;
+        const player1Pits = pits.slice(0, (pits.length / 2) - 1);
+        const player1Bank = pits[(pits.length / 2) - 1];
+        const player2Pits = pits.slice(pits.length / 2, pits.length - 1);
+        const player2Bank = pits[pits.length - 1];
         return (
             <div className={'board'}>
-                <div className={'playground'}>
-                    <div className={'endsection'}>
-                        <Bank pit={player2Bank}></Bank>
-                    </div>
-                    <div className={'midsection'} flex-grow={numberOfPitsForEachPlayer}>
-                        <div className={'midrow'}>
-                            {player2Pits.reverse().map((pit,key) => <Pit key={key} pit={pit} move={move}></Pit>)}
+                {numberOfPlayers <= 2 ? (
+                    <div className={'playground'}>
+                        <div className={'endsection'}>
+                            <Bank pit={player2Bank}></Bank>
                         </div>
-                        <div className={'midrow'}>
-                            {player1Pits.map((pit,key) => <Pit key={key} pit={pit} move={move}></Pit>)}
+                        <div className={'midsection'} flex-grow={numberOfPitsForEachPlayer}>
+                            <div className={'midrow'}>
+                                {player2Pits.reverse().map((pit, key) => <Pit key={key} pit={pit} move={move}></Pit>)}
+                            </div>
+                            <div className={'midrow'}>
+                                {player1Pits.map((pit, key) => <Pit key={key} pit={pit} move={move}></Pit>)}
+                            </div>
                         </div>
+                        <div className={'endsection'}>
+                            <Bank pit={player1Bank}></Bank>
+                        </div>
+                    </div>) : (
+                    <div className={'error'}>
+                        <h1 className={'error-message'}>Unfortunately UI only supports up to two players</h1>
                     </div>
-                    <div className={'endsection'}>
-                        <Bank pit={player1Bank}></Bank>
-                    </div>
-                </div>
+                )}
             </div>
         );
     }
